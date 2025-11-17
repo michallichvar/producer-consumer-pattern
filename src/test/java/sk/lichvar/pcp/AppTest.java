d81ab4ded71043a09aaa1117de449d2052fb6680
lichvar <michal.lichvar@geneton.sk>
2025-11-17 15:45:14 +0100

    Added simple test scenarios, both positive and negative.

--- src/test/java/sk/lichvar/pcp/AppTest.java
+++ src/test/java/sk/lichvar/pcp/AppTest.java
@@ -1,103 +1,152 @@
 package sk.lichvar.pcp;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import sk.lichvar.pcp.commands.AddCommand;
-import sk.lichvar.pcp.commands.Command;
-import sk.lichvar.pcp.commands.DeleteAllCommand;
-import sk.lichvar.pcp.commands.PrintAllCommand;
-import sk.lichvar.pcp.pattern.Consumer;
-import sk.lichvar.pcp.pattern.Producer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sk.lichvar.pcp.consumer.Consumer;
+import sk.lichvar.pcp.producer.Producer;
 import sk.lichvar.pcp.queue.CommandQueue;
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import static org.mockito.Mockito.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 /**
- * Several tests, but all of them can be evaluated just by personally checking the console output
+ * Several test scenarios
  * <ul>
- *     <li>{@link #testAssignedTaskSolution} solution for assigned task</li>
- *     <li>{@link #testQueueFull} tests scenario when queue is shorter then number of input commands</li>
- *     <li>{@link #testConsumerProcessingException} tests scenario when processing command in consumer throws Exception</li>
+ *     <li>{@link #testInputFileProcessed} positive scenario, tests system output for printed users.</li>
+ *     <li>{@link #testResourceFileNotFound} negative scenario, tests system error for logger message: "Error in producer."</li>
+ *     <li>{@link #testResourceFileEmpty} positive scenario, tests system output does not contain printed users.</li>
+ *     <li>{@link #testAddCommandMalformed} negative scenario, tests system error for logger message: "Error in producer." and "NumberFormatException"</li>
+ *     <li>{@link #testWrongCommand} negative scenario, tests system error for logger message: "Error in producer." and wrong command named "UpdateAll"</li>
  * </ul>
  */
 public class AppTest {
 
-	private void test(int capacity, Queue<Command> inputCommands) throws InterruptedException {
-		CommandQueue queue = new CommandQueue(capacity);
+	private static final Logger LOGGER = LoggerFactory.getLogger(AppTest.class);
 
-		Thread consumerThread = new Thread(new Consumer(queue));
-		consumerThread.start();
+	private final ByteArrayOutputStream spyOut = new ByteArrayOutputStream();
+	private final ByteArrayOutputStream spyErr = new ByteArrayOutputStream();
 
-		Thread producerThread = new Thread(new Producer(queue, inputCommands));
+	private final PrintStream originalOut = System.out;
+	private final PrintStream originalErr = System.err;
+
+	private static CommandQueue queue;
+	private static Thread consumerThread;
+	private static Thread producerThread;
+
+	@BeforeEach
+	public void setUpStreams() {
+		System.setOut(new PrintStream(spyOut));
+		System.setErr(new PrintStream(spyErr));
+	}
+
+	@AfterEach
+	public void restoreStreams() {
+		System.setOut(originalOut);
+		System.setErr(originalErr);
+	}
+
+	@Test
+	public void testInputFileProcessed() {
+		queue = new CommandQueue(50);
+
+		producerThread = new Thread(new Producer("/input.cmd", queue));
 		producerThread.start();
 
-		consumerThread.join();
+		consumerThread = new Thread(new Consumer(queue));
+		consumerThread.start();
+
+		joinProducerThread();
+		joinConsumerThread();
+
+		Assertions.assertThat(spyOut.toString())
+				.contains("User[userId=1,userGuid=a1,userName=Robert)]")
+				.contains("User[userId=2,userGuid=a2,userName=Martin)]");
 	}
 
-	/**
-	 * Runs assigned task solution
-	 * @throws InterruptedException
-	 */
 	@Test
-	public void testAssignedTaskSolution() throws InterruptedException {
-		// non tread safe queue for input commands (as they are all processed in one thread)
-		Queue<Command> inputCommands = Stream.of(
-				new AddCommand(1, "a1", "Robert"),
-				new AddCommand(2, "a2", "Martin"),
-				new PrintAllCommand(),
-				new DeleteAllCommand(),
-				new PrintAllCommand()
-		).collect(Collectors.toCollection(LinkedList::new));
+	public void testResourceFileNotFound() {
+		queue = new CommandQueue(50);
 
-		test(50, inputCommands);
+		producerThread = new Thread(new Producer("/nonexistent.cmd", queue));
+		producerThread.start();
+
+		joinProducerThread();
+
+		Assertions.assertThat(spyErr.toString())
+						.contains("Error in producer.")
+						.contains("nonexistent.cmd");
 	}
 
-	/**
-	 * Tests producer waiting scenario
-	 * @throws InterruptedException
-	 */
 	@Test
-	public void testQueueFull() throws InterruptedException {
-		// non tread safe queue for input commands (as they are all processed in one thread)
-		Queue<Command> inputCommands = Stream.of(
-				new AddCommand(1, "a1", "Robert"),
-				new AddCommand(2, "a2", "Martin"),
-				new AddCommand(3, "a3", "Juraj"),
-				new AddCommand(4, "a4", "Peter"),
-				new AddCommand(5, "a5", "Vasil"),
-				new AddCommand(6, "a6", "George"),
-				new AddCommand(7, "a7", "Samara"),
-				new AddCommand(8, "a8", "Tamara"),
-				new PrintAllCommand(),
-				new DeleteAllCommand(),
-				new PrintAllCommand()
-		).collect(Collectors.toCollection(LinkedList::new));
+	public void testResourceFileEmpty() {
+		queue = new CommandQueue(50);
 
-		test(5, inputCommands);
+		producerThread = new Thread(new Producer("/tests/empty.cmd", queue));
+		producerThread.start();
+
+		consumerThread = new Thread(new Consumer(queue));
+		consumerThread.start();
+
+		joinProducerThread();
+		joinConsumerThread();
+
+		Assertions.assertThat(spyOut.toString())
+				.doesNotContain("User[");
 	}
 
-	/**
-	 * Tests consumer processing exception
-	 * @throws InterruptedException
-	 */
 	@Test
-	public void testConsumerProcessingException() throws InterruptedException {
-		AddCommand addCommand = mock(AddCommand.class);
-		doThrow(new RuntimeException("Cannot connect to DB")).when(addCommand).execute();
+	public void testAddCommandMalformed() {
+		queue = new CommandQueue(50);
 
-		// non tread safe queue for input commands (as they are all processed in one thread)
-		Queue<Command> inputCommands = Stream.of(
-				new AddCommand(1, "a1", "Robert"),
-				addCommand,
-				new PrintAllCommand(),
-				new DeleteAllCommand(),
-				new PrintAllCommand()
-		).collect(Collectors.toCollection(LinkedList::new));
+		producerThread = new Thread(new Producer("/tests/add_command_malformed.cmd", queue));
+		producerThread.start();
 
-		test(5, inputCommands);
+		joinProducerThread();
+
+		Assertions.assertThat(spyErr.toString())
+				.contains("Error in producer.")
+				.contains("java.lang.NumberFormatException: For input string: \"\"1\"\"");
+	}
+
+	@Test
+	public void testWrongCommand() {
+		queue = new CommandQueue(50);
+
+		producerThread = new Thread(new Producer("/tests/wrong_command.cmd", queue));
+		producerThread.start();
+
+		joinProducerThread();
+
+		Assertions.assertThat(spyErr.toString())
+				.contains("Error in producer.")
+				.contains("java.text.ParseException: Unknown command: UpdateAll");
+	}
+
+	private static void joinProducerThread() {
+		try {
+			producerThread.join();
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			LOGGER.warn("Main thread interrupted while waiting for producer thread to finish.");
+			if (LOGGER.isDebugEnabled()) {
+				LOGGER.debug(e.getMessage(), e);
+			}
+		}
+	}
+
+	private static void joinConsumerThread() {
+		try {
+			consumerThread.join();
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			LOGGER.warn("Main thread interrupted while waiting for consumer thread to finish.");
+			if (LOGGER.isDebugEnabled()) {
+				LOGGER.debug(e.getMessage(), e);
+			}
+		}
 	}
 }
